82126a6158999562f218d833f725fab4d7e0781d
“William <williamwildernash@gmail.com>
2022-12-26 17:45:33 -0500

    ssl 6th try

--- serverIndex.js
+++ serverIndex.js
@@ -3,23 +3,23 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const fs = require('fs')
-const https = require('https')
-const http = require('http')
+//const https = require('https')
+//const http = require('http')
 
 const loaders = require('./server/loaders');
 
 const httpsPORT = process.env.PORT || 443;
-const httpPORT = process.env.PORT || 80;
-const hostname = 'freelancev1.com'
+//const httpPORT = process.env.PORT || 80;
+//const hostname = 'freelancev1.com'
 
-const httpsOptions = {
+/*const httpsOptions = {
   cert: fs.readFileSync('server/freelancev1_com.crt','utf8'),
   ca: [fs.readFileSync('server/freelancev1_com1.ca-bundle', 'utf8'),fs.readFileSync('server/freelancev1_com2.ca-bundle','utf8')],
   key: fs.readFileSync('server/freelancev1_com.key', 'utf8')
-}
+}*/
 
-const httpServer = http.createServer(app)
-const httpsServer = https.createServer(httpsOptions, app)
+//const httpServer = http.createServer(app)
+//const httpsServer = https.createServer(httpsOptions, app)
 
 async function startServer() {
   // Init application loaders
@@ -42,14 +42,14 @@ async function startServer() {
   app.use('/facebook', express.static(path.join(__dirname, 'build')))
   app.use('/linkedin', express.static(path.join(__dirname, 'build')))
 
-  httpsServer.listen(httpsPORT)
+  app.listen(httpsPORT)
   }
 
-  if(process.env.REACT_APP_NODE_ENV === 'development'){
+  /*if(process.env.REACT_APP_NODE_ENV === 'development'){
     httpServer.listen(httpPORT, function(){
       console.log(`Server is running on ${httpPORT}`)
     });
-  }
+  }*/
 
 }
 
