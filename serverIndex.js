d244a4845a9a4efb01e7c36ddb319404ce2d9171
“William <williamwildernash@gmail.com>
2022-12-26 14:17:22 -0500

    first SSL commit

--- serverIndex.js
+++ serverIndex.js
@@ -2,15 +2,37 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const path = require('path')
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
 
 const loaders = require('./server/loaders');
 
-const PORT = process.env.PORT || 80
+const httpsPORT = process.env.PORT || 443;
+const httpPORT = process.env.PORT || 80;
+const hostname = 'freelancev1.com'
+
+const httpsOptions = {
+  cert: fs.readFileSync('server/freelancev1_com.crt','utf8'),
+  ca: [fs.readFileSync('server/freelancev1_com1.ca-bundle', 'utf8'),fs.readFileSync('server/freelancev1_com2.ca-bundle','utf8')],
+  key: fs.readFileSync('server/freelancev1_com.key', 'utf8')
+}
+
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(httpsOptions, app)
 
 async function startServer() {
-
   // Init application loaders
   loaders(app);
+
+  if(process.env.REACT_APP_NODE_ENV === "production"){
+  app.use((req, res, next) => {
+    if(req.protocol === 'http'){
+      res.redirect(301, `https://${req.headers.host}${req.url}`)
+    }
+    next()
+  })
+  }
 
   if(process.env.REACT_APP_NODE_ENV === "production"){
   app.use('/', express.static(path.join(__dirname, 'build')))
@@ -24,10 +46,15 @@ async function startServer() {
   app.use('/linkedin', express.static(path.join(__dirname, 'build')))
   }
 
-  // Start server
-  app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`);
-  })
+  if(process.env.REACT_APP_NODE_ENV === "production"){
+  httpsServer.listen(httpsPORT, hostname)
+  }
+  if(process.env.REACT_APP_NODE_ENV === 'development'){
+    httpServer.listen(httpPORT, function(){
+      console.log(`Server is running on ${httpPORT}`)
+    });
+  }
+
 }
 
-startServer();
+startServer()
